7fdc8da093531a5fc5e8e805e4656062f1f70539
SerjAyvazyan-2000 <ayvazyanserj3@gmail.com>
2025-01-10 15:39:06 +0400

    new

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -24,12 +24,13 @@
 
 
 gsap.registerPlugin(ScrollTrigger);
-const isMobile = window.innerWidth < 1200;
+const isTablet = window.innerWidth < 1200;
+
 
 gsap.fromTo('.possibilities-item--one',
     { y: 0 },
     {
-        y: isMobile ? -40 : 0,
+        y: isTablet ? -40 : 0,
         scrollTrigger: {
             trigger: '.possibilities-section',
             start: "top top",
@@ -42,7 +43,7 @@ gsap.fromTo('.possibilities-item--one',
 gsap.fromTo('.possibilities-item--two',
     { y: 0 },
     {
-        y: isMobile ? -443 : -370,
+        y: isTablet ? -443 : -370,
         scrollTrigger: {
             trigger: '.possibilities-section',
             start: "top+=100 top",
@@ -55,7 +56,7 @@ gsap.fromTo('.possibilities-item--two',
 gsap.fromTo('.possibilities-item--tree',
     { y: 0 },
     {
-        y: isMobile ? -846 : -740,
+        y: isTablet ? -846 : -740,
         scrollTrigger: {
             trigger: '.possibilities-section',
             start: "top+=200 top",
@@ -68,7 +69,9 @@ gsap.fromTo('.possibilities-item--tree',
 gsap.fromTo('.possibilities-item--for',
     { y: 0 },
     {
-        y: isMobile ? -1248 : -1115,
+        y: isTablet ? -1248 : -1115 ? isMobile : 1500,
+        y: isTablet ? -1248 : -1115 ? isMobile : 1500,
+
         scrollTrigger: {
             trigger: '.possibilities-section',
             start: "top+=300 top",
@@ -81,7 +84,7 @@ gsap.fromTo('.possibilities-item--for',
 gsap.fromTo('.possibilities-item--five',
     { y: 0 },
     {
-        y: isMobile ? -1650 : -1480,
+        y: isTablet ? -1650 : -1480,
         scrollTrigger: {
             trigger: '.possibilities-section',
             start: "top+=400 top",
@@ -94,7 +97,7 @@ gsap.fromTo('.possibilities-item--five',
 gsap.fromTo('.possibilities-item--six',
     { y: 0 },
     {
-        y: isMobile ? -2054 : -1850, // Увеличиваем на 200 для мобильных
+        y: isTablet ? -2054 : -1850, // Увеличиваем на 200 для мобильных
         scrollTrigger: {
             trigger: '.possibilities-section',
             start: "top+=500 top",
