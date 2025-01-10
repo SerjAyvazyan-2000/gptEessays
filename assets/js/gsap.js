d1f8038f151d41faa415320f8e97054358d3da90
SerjAyvazyan-2000 <ayvazyanserj3@gmail.com>
2025-01-11 00:52:40 +0400

    new

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -1,31 +1,7 @@
 
 
-
-// gsap.registerPlugin(ScrollTrigger);
-//
-// // Для каждой карточки добавляем анимацию
-// gsap.utils.toArray('.possibilities-animate').forEach((item, index) => {
-//     gsap.fromTo(item,
-//         {
-//             y: 0, // Начальная позиция карточки
-//         },
-//         {
-//             y: -350 * index, // Каждая карточка сдвигается точно на свою высоту
-//             scrollTrigger: {
-//                 trigger: '.possibilities-section', // Общий триггер для всех карточек
-//                 start: () => `top ${20 - index * 20}%`, // Каждая карточка начинает позже предыдущей
-//                 end: () => `+=${350}`, // Каждая карточка анимируется на 350px
-//                 scrub: true, // Плавная анимация
-//                 toggleActions: "play reverse play reverse", // Анимация запускается в обе стороны
-//             }
-//         }
-//     );
-// });
-
-
 gsap.registerPlugin(ScrollTrigger);
 const isTablet = window.innerWidth < 1200;
-
 
 gsap.fromTo('.possibilities-item--one',
     { y: 0 },
@@ -40,18 +16,40 @@ gsap.fromTo('.possibilities-item--one',
     }
 );
 
+gsap.to('.possibilities-item--one', {
+    scale: 0.9,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: '.possibilities-item--two', // Следующая карточка
+        start: "top+=60% center", // Начало перекрытия на 60%
+        end: "top+=50% center", // Конец перекрытия на 80%
+        scrub: true,
+    }
+});
+
 gsap.fromTo('.possibilities-item--two',
     { y: 0 },
     {
         y: isTablet ? -443 : -370,
         scrollTrigger: {
             trigger: '.possibilities-section',
-            start: "top+=100 top",
+            start: "top top",
             end: "top+=200 top",
             scrub: true,
         }
     }
 );
+
+gsap.to('.possibilities-item--two', {
+    scale: 0.9,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: '.possibilities-item--tree', // Следующая карточка
+        start: "top+=0% center", // Начало перекрытия на 60%
+        end: "top+=20 center", // Конец перекрытия на 80%
+        scrub: true,
+    }
+});
 
 gsap.fromTo('.possibilities-item--tree',
     { y: 0 },
@@ -66,25 +64,45 @@ gsap.fromTo('.possibilities-item--tree',
     }
 );
 
+gsap.to('.possibilities-item--tree', {
+    scale: 0.9,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: '.possibilities-item--for', // Следующая карточка
+        start: "top-=320 center", // Начало перекрытия на 60%
+        end: "top+=40 center", // Конец перекрытия на 80%
+        scrub: true,
+    }
+});
+
 gsap.fromTo('.possibilities-item--for',
     { y: 0 },
     {
-        y: isTablet ? -1248 : -1115 ? isMobile : 1500,
-        y: isTablet ? -1248 : -1115 ? isMobile : 1500,
-
+        y: isTablet ? -1248 : -1120,
         scrollTrigger: {
             trigger: '.possibilities-section',
-            start: "top+=300 top",
+            start: "top+=200 top",
             end: "top+=400 top",
             scrub: true,
         }
     }
 );
 
+gsap.to('.possibilities-item--for', {
+    scale: 0.9,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: '.possibilities-item--five', // Следующая карточка
+        start: "top-=650 center", // Начало перекрытия на 60%
+        end: "top+=50% center", // Конец перекрытия на 80%
+        scrub: true,
+    }
+});
+
 gsap.fromTo('.possibilities-item--five',
     { y: 0 },
     {
-        y: isTablet ? -1650 : -1480,
+        y: isTablet ? -1650 : -1490,
         scrollTrigger: {
             trigger: '.possibilities-section',
             start: "top+=400 top",
@@ -94,28 +112,119 @@ gsap.fromTo('.possibilities-item--five',
     }
 );
 
+gsap.to('.possibilities-item--five', {
+    scale: 0.9,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: '.possibilities-item--six', // Следняя карточка
+        start: "top-=850 center", // Начало перекрытия на 60%
+        end: "top+=50% center", // Конец перекрытия на 80%
+        scrub: true,
+    }
+});
+
 gsap.fromTo('.possibilities-item--six',
     { y: 0 },
     {
-        y: isTablet ? -2054 : -1850, // Увеличиваем на 200 для мобильных
+        y: isTablet ? -2054 : -1860,
         scrollTrigger: {
             trigger: '.possibilities-section',
-            start: "top+=500 top",
+            start: "top+=600 top",
             end: "top+=600 top",
             scrub: true,
         }
     }
 );
 
+gsap.to('.possibilities-item--six', {
+    scrollTrigger: {
+        trigger: '.possibilities-section', // Нет следующей карточки
+        start: "top+=60% center", // Начало перекрытия на 60%
+        end: "top+=80% center", // Конец перекрытия на 80%
+        scrub: true,
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// gsap.fromTo('.possibilities-item--for',
+//     { y: 0 },
+//     {
+//         y: isTablet ? -1248 : -1115 ,
+//         scrollTrigger: {
+//
+//             trigger: '.possibilities-section',
+//             start: "top+=300 top",
+//             end: "top+=400 top",
+//             scrub: true,
+//         }
+//     }
+// );
+//
+//
+//
+//
+//
+// gsap.fromTo('.possibilities-item--five',
+//     { y: 0 },
+//     {
+//         y: isTablet ? -1650 : -1480,
+//         scrollTrigger: {
+//             trigger: '.possibilities-section',
+//             start: "top+=400 top",
+//             end: "top+=500 top",
+//             scrub: true,
+//         }
+//     }
+// );
+//
+// gsap.fromTo('.possibilities-item--six',
+//     { y: 0 },
+//     {
+//         y: isTablet ? -2054 : -1850, // Увеличиваем на 200 для мобильных
+//         scrollTrigger: {
+//             trigger: '.possibilities-section',
+//             start: "top+=500 top",
+//             end: "top+=600 top",
+//             scrub: true,
+//         }
+//     }
+// );
+
+
+
+
+
+
+
+
+
+
 
 gsap.to('.possibilities-section', {
     scrollTrigger: {
         trigger: '.possibilities-section',
         start: "top 4%",
-
-        end: "bottom top", // Окончание — когда нижняя часть секции выходит за экран
+        end: "bottom top",
         scrub: true,
         pin: true,
         pinSpacing: false,
     }
-});
+
+})
